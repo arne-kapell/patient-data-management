64b96af1ccc61cde15e186b2c51991c5a3e7a093
Arne Kapell <arne.kapell@pm.me>
2022-12-04 22:32:59 +0100

    secure filenames

--- pdm/views.py
+++ pdm/views.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+import werkzeug
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.views.decorators.csrf import csrf_protect
@@ -66,6 +67,7 @@ def upload(request):
                 try:
                     tmp_path = "documents/tmp/"
                     tmp = f"{tmp_path}exif.{file_format}"
+                    tmp = werkzeug.utils.secure_filename(tmp)
                     os.mkdir("documents/tmp")
                     with open(tmp, "wb") as f:
                         efile = EncryptedFile(doc.file).read()
